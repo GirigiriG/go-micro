affddb35597b84703e80e17585b8f3eba02ff9a2
Gideon Girigiri <gideongirigiri@gamil.com>
2022-12-02 09:28:16 -0500

    initial commit

---
+++ broker-service/cmd/api/routes.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// Who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"POST", "GET", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-TOKEN"},
+		ExposedHeaders:   []string{"Links"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Post("/send", app.Broker)
+
+	return mux
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,8 @@
+module github.com/girigirig/broker
+
+go 1.19
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/cors v1.2.1
+)
